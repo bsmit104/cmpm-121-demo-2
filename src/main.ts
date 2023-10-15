e9ab5aaa13e469cff2e4f7012f0809188fc70ae9
r2 <jbrayden35@gmail.com>
2023-10-14 23:23:01 -0700

    step 2 complete: added mouse drawing

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,42 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 container.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+//const paths = [];
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  // ctx null check
+  if (cursor.active && ctx) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+// add to container so vertical
+container.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (ctx) { // ctx null check
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
