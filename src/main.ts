0d063f32c5b9660a435d81df23e2a0e2ea5060ae
r2 <jbrayden35@gmail.com>
2023-10-14 23:13:41 -0700

    step 1 complete: added canvas and header

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Paint";
 
 document.title = gameName;
 
+const container = document.createElement("div");
+app.append(container);
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+container.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+container.append(canvas);
