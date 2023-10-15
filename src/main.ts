1717752ad1c3c8201abe39548eab3ec0d2d3e3d0
r2 <jbrayden35@gmail.com>
2023-10-14 23:48:58 -0700

    step 3 complete: added array of points and dispatch events

--- src/main.ts
+++ src/main.ts
@@ -20,39 +20,115 @@ container.append(canvas);
 
 const ctx = canvas.getContext("2d");
 
+const lines: { x: number; y: number }[][] = [];
+const redoLines: { x: number; y: number }[][] = [];
+
+let currentLine: { x: number; y: number }[] | null = null;
+
 const cursor = { active: false, x: 0, y: 0 };
 
-//const paths = [];
+const canvasEventTarget = new EventTarget();
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  redraw();
+
+  canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  // ctx null check
-  if (cursor.active && ctx) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+  if (cursor.active && currentLine) {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    redraw();
+
+    canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
+
+  redraw();
+
+  canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
 });
 
+function redraw() {
+  if (ctx) {
+    // ctx null check
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const line of lines) {
+      if (line.length > 1) {
+        ctx.beginPath();
+        const { x, y } = line[0];
+        ctx.moveTo(x, y);
+        for (const { x, y } of line) {
+          ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+document.body.append(document.createElement("br"));
+
 const clearButton = document.createElement("button");
-clearButton.innerHTML = "Clear";
-// add to container so vertical
+clearButton.innerHTML = "clear";
 container.append(clearButton);
 
 clearButton.addEventListener("click", () => {
-  if (ctx) { // ctx null check
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
+  redraw();
+
+  canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
+});
+
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+container.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    const poppedLine = lines.pop();
+    if (poppedLine) {
+      redoLines.push(poppedLine);
+      redraw();
+
+      canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
+    }
   }
+});
+
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
+container.append(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    const poppedRedoLine = redoLines.pop();
+    if (poppedRedoLine) {
+      lines.push(poppedRedoLine);
+      redraw();
+
+      canvasEventTarget.dispatchEvent(new Event("drawing-changed"));
+    }
+  }
+});
+
+// Add an observer for the "drawing-changed" event to clear and redraw lines
+canvasEventTarget.addEventListener("drawing-changed", () => {
+  redraw();
 });
